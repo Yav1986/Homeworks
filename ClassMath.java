74b14967bcb2f16eefb44d08bbb52eeeb98f2a7b
Yav1986 <100689241+Yav1986@users.noreply.github.com>
2022-03-11 14:55:45 +0200

    Add files via upload

---
+++ ClassMath.java
@@ -0,0 +1,14 @@
+package Math;
+
+public class ClassMath {
+    public static void main(String[] args){
+        //Problems 2-5
+        System.out.println(2+7);
+        System.out.println(Math.abs(-147) +"\t"+(Math.abs(15)+"\t"+(Math.abs(0))+"\t"+ (Math.abs(18)) ));
+      System.out.println(Math.pow(-20,2)+"\t"+ Math.pow(90,2)+"\t"+ Math.pow(680,2)
+              +"\t"+ Math.pow(73,2)+"\t"+ Math.pow(200,2)+"\t"+ Math.pow(-22,2));
+        System.out.println(Math.sqrt(8)+"\t"+ Math.sqrt(353)+"\t"+ Math.sqrt(78)+"\t"+ Math.sqrt(3)
+                +"\t"+ Math.sqrt(-67)+"\t"+ Math.sqrt(723)+"\t"+ Math.sqrt(2000)+"\t"+ Math.sqrt(-100));
+    }
+
+}
---
+++ MyName.java
@@ -0,0 +1,11 @@
+package myfirstpack;
+
+public class MyName {
+    public static void main(String[] args){
+        //Zadacha 1
+        String MyName="Yavor";
+        System.out.println(MyName.length());
+        System.out.println(MyName.toLowerCase());
+        System.out.println(MyName.toUpperCase());
+    }
+}
